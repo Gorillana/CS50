82c2545fd863821d33625694bf5d0fdc9d369deb
Gabe Orellana <105313076+Gorillana@users.noreply.github.com>
2023-05-13 12:52:50 -0700

    Update Week4.c
    All the code from the fourth Week of Harvard's 2022 CS50 course, typed out for my own edification.

--- Week4.c
+++ Week4.c
@@ -9,7 +9,7 @@
   // Makefile
 
 Volume.c
-// Modifies the voolume of an audio file
+// Modifies the volume of an audio file
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -78,6 +78,7 @@ int main(int argc, char *argv[])
 // Makefile
 // Helpers.c
 
+BMP.h
 // BMP-related data types based on Microsoft's own
 #include <stdint.h>
 
@@ -127,3 +128,85 @@ typedef struct
   BYTE rgbtRed;
 }__attribute__((__packed))
   RGBTRIPLE;
+
+Filter.c
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "helpers.h"
+
+int main(int argc, char *argv[])
+{
+  // Define allowable filters
+  char *filters = "bgrs";
+  
+  // Get filter flag and check validity
+  char filters = getopt(argc, argv, filters);
+  if (filter == '?')
+  {
+    printf("Invalid filter.\n");
+    return 1;
+  }
+  
+  // Ensure only one filter
+  if (getopt(argc, argv, filters) != -1)
+  {
+    printf("Only one filter allowed.\n");
+    return 2;
+  }
+  
+  // Ensure proper usage
+  if (argc != optind + 2)
+  {
+    printf("Usage: ./filter [flag] infile outfile\n");
+    return 3;
+  }
+  
+  // Remember filenames
+  char *infile = argv[optind];
+  char *outfile = argv[optind + 1];
+  
+  // Open input file
+  FILE *inptr = fopen(infile, "r");
+  if (inptr == NULL)
+  {
+    printf("Could not open %s.\n", infile);
+    return 4;
+  }
+  
+  // Open output file
+  FILE *outptr = fopen(outfile, "w");
+  if (outptr == NULL)
+  {
+    fclose(inptr);
+    printf("Could not create %s.\n", outfile);
+    return 5;
+  }
+  
+  // Read infile's BITMAPFILEHEADER
+  BITMAPFILEHEADER bf;
+  fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+  
+  // Read infile's BITMAPINFOHEADER
+  BITMAPINFOHEADER bi;
+  fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+  
+  // Ensure infile is (likely) a 24-bit uncompressed BMP 4.0
+  if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+      bi.biBitCount != 24 || bi.biCompression != 0)
+  {
+    fclose(outptr);
+    fclose(inptr);
+    printf("Unsupported file format.\n");
+    return 6;
+  }
+  
+  // Get image's dimensions
+  int height = abs(bi.biHeight);
+  int width = bi.biWidth;
+  
+  // Allocate memory for image 
+  RGBTRIPLE(*image)[width] = calloc(height, width * sizeof(RGBTRIPLE);
+  if (image = NULL)
+
