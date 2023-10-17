62bebf7ce6ae3d73eb1affa2d28af9200b908f39
Gabe Orellana <105313076+Gorillana@users.noreply.github.com>
2023-10-16 23:57:26 -0700

    Update app.py

--- Week9/PSets/Finance/app.py
+++ Week9/PSets/Finance/app.py
@@ -90,9 +90,45 @@ def buy():
           "SELECT cash FROM users WHERE id = :user_id", user_id = session["user_id"]
       )[0]["cash"]
 
+      # Compare current user cash to cost of stocks
+      if cash < total_cost:
+          return apology("Insufficient funds")
+
+      # Update users table
+      db.execute(
+          "UPDATE users SET cash = cash - :total_cost WHERE id = :user_id",
+          total_cost=total_cost,
+          user_id=session["user_id"],
+      )
+
+      # Add the purchase to the history table
+      db.execute(
+            "INSERT INTO transactions (user_id, symbol, shares, price, type) VALUES (:user_id, :symbol, :shares, :price, :type)",
+            user_id=session["user_id"],
+            symbol=symbol,
+            shares=shares,
+            price=price,
+            type="Buy",
+        )
+
+      flash(f"Bought {shares} shares of {symbol} for {usd(total_cost)}!")
+      return redirect("/")
+
+  else:
+      return render_template("buy.html")
+
 @app.route("/history")
 @login_required
 def history():
+  """Show history of transactions"""
+  # Query database for user's transactions, ordered by most recent first
+  transactions = db.execute(
+      "SELECT * FROM transactions WHERE user_id = :user_id ORDER BY timestamp DESC",
+      user_id=session["user_id"],
+      )
+
+  # Render history page with transactions
+  return render_template("history.html", transactions=transactions)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -170,7 +206,6 @@ def quote():
           symbol=stock["symbol"],
       )
 
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
   """ Register user"""
@@ -223,4 +258,103 @@ def register():
 @login_required
 def sell():
   """ Sell shares of stock"""
-  # Get user's 
+  # Get user's stocks
+  stocks = db.execute(
+      "SELECT symbol, SUM(shares) AS total_shares FROM transactions WHERE user_id = :user_id GROUP BY symbol HAVING total_shares > 0",
+      user_id=session["user_id"],
+      )
+
+  # If the user submits the form
+  if request.method == "POST":
+      #create vars for if statements
+      symbol = request.form.get("symbol").upper()
+      shares = request.form.get("shares")
+      if not symbol:
+          return apology("Must provide symbol", 400)
+      elif not shares or not shares.isdigit() or int(shares) <= 0:
+          return apology("Must provide a positive number of shares", 400)
+      else:
+          shares = int(shares)
+
+      # cycle through the stocks user has purchased
+      for stock in stocks:
+          if stock["symbol"] == symbol:
+              if stock["total_shares"] < shares:
+                  return apology("Not enough shares", 400)
+              else:
+                  # Get quote
+                  quote = lookup(symbol)
+                  if quote is None:
+                      return apology("Symbol not found", 400)
+                  price = quote["price"]
+                  total_sale = shares * price
+
+                  # Update users table
+                  db.execute(
+                      "UPDATE users SET cash = cash + :total_sale WHERE id = :user_id",
+                      total_sale=total_sale,
+                      user_id=session["user_id"],
+                      )
+
+                  # Add the sale to the history table
+                  db.execute(
+                      "INSERT INTO transactions (user_id, symbol, shares, price, type) VALUES (:user_id, :symbol, :shares, :price, :type)",
+                      user_id=session["user_id"],
+                      symbol=symbol,
+                      shares=(-1) * shares,
+                      price=price,
+                      type="Sell,
+                      )
+
+                  flash(f"Sold {shares} shares of {symbol} for {usd(total_sale)}!")
+                  return redirect("/")
+
+      return apology("Nonexistent symbol", 400)
+
+   # If the user visits the page
+   else:
+      return render_template("sell.html", stocks=stocks)
+
+# Personal touch, allow user to change password
+@app.route("/password", methods=["GET", "POST"])
+@login_required
+def password():
+    """ Allow user to change password"""
+
+    # get current user
+    user_id = session["user_id"]
+
+    if request.method == "POST":
+        # create variables for if statements
+        current_pwd = request.form.get("current-password")
+        confirmation = request.form.get("current-confirmation")
+        new_pwd = request.form.get("new-password")
+        new_conf = request.form.get("new-confirmation")
+
+        search_id = db.execute("SELECT * FROM users WHERE id = ?", user_id)
+
+        if not current_pwd:
+            return apology("Missing password!", 400)
+        if not confirmation:
+            return apology("Provide confirmation!", 400)
+        if current_pwd != confirmation:
+            return apology("Password and confirmation don't match!", 400)
+        if not new_pwd:
+            return apology("missing new password!", 400)
+        if new_pwd != new_conf:
+            return apology("New password and new confirmation don't match!", 400)
+        if not check_password_hash(
+            search_id[0]["hash"], request.form.get("current-password")
+        ):
+            return apology("Invalid password.", 400)
+
+        hash_pd = generate_password_hash(new_pwd, method="pbdkf2:sha256", salt_length=8)
+
+        db.execute("UPDATE users SET hash = ? WHERE id = ?", hash_pd, user_id)
+
+        return redirect("/")
+
+    # User arrived via GET method
+    else:
+        return render_template("password.html")
+  
